9020bba5ec52b111a972c2ff39e00d538e3e073c
InvisibleFriend71 <ss.stabby@gmail.com>
2012-11-02 23:25:53 -0400

    part 2 section 3 of xenses

--- twitter.py
+++ twitter.py
@@ -3,16 +3,27 @@ import simplejson as json
 import sys
 
 def search_twitter(query='python'):
-	url = 'http://search.twitter.com/search.json?q=' + query
+	url = 'http://search.twitter.com/search.json?show_user=true&q=' + query
 	response = urllib.urlopen(url).read()
 	data = json.loads(response)
 	return data['results']
 	
 def print_tweets(tweets):
   for tweet in tweets:
-    print tweet['from_user'] + ': ' + tweet['text'] + '\n'
+    print tweet['text'] + '\n'
 
 if __name__ == "__main__":
   query = sys.argv[1]
   results = search_twitter(query)
+  print_tweets(results)
+
+def search_user(query):
+	url = 'http://search.twitter.com/search.json?showuser=true&q=' + query
+	response = urllib.urlopen(url).read()
+	data = json.loads(response)
+	return data['results']
+	
+if __name__ == "__main__":
+  query = sys.argv[1]
+  results = search_user(query)
   print_tweets(results)
